4af4468ea43f72dbad57699bbcab3ceaad963a76
RetchBlade <118535671+RetchBlade@users.noreply.github.com>
2024-01-16 20:20:38 +0100

    Aufgabe 7 Altpraktika
    Auch Tutorium Uebung

---
+++ VorbereitungJava1/src/Aufgabe7/NormSparvertrag.java
@@ -0,0 +1,32 @@
+package Aufgabe7;
+
+public class NormSparvertrag extends Sparvertrag implements Zinsen{
+	private double guthaben;
+	public NormSparvertrag(String sparer, double guthaben) {
+		super(sparer, "Normaler Sparvertrag");
+		// TODO Auto-generated constructor stub
+		this.setGuthaben(guthaben);
+		
+	}
+	
+	public double getGuthaben() {
+		return guthaben;
+	}
+	
+	void setGuthaben(double guthaben) {
+		this.guthaben = guthaben;
+	}
+
+	@Override
+	public void addZins(int dauer) {
+		// TODO Auto-generated method stub
+		int w = 3;
+		if(dauer <= w){
+			this.guthaben = (guthaben*ZINS1/100) * dauer + guthaben; 
+		} else {
+			this.guthaben = (guthaben*ZINS2/100) * dauer + guthaben; 
+		}
+	}
+	
+	
+}
---
+++ VorbereitungJava1/src/Aufgabe7/Sparvertrag.java
@@ -0,0 +1,27 @@
+package Aufgabe7;
+
+public class Sparvertrag {
+	private String sparer, vertragTyp;
+
+	public Sparvertrag(String sparer, String vertragTyp) {
+		this.setSparer(sparer);
+		this.setVertragTyp(vertragTyp);
+	}
+
+	public String getSparer() {
+		return sparer;
+	}
+
+	void setSparer(String sparer) {
+		this.sparer = sparer;
+	}
+
+	public String getVertragTyp() {
+		return vertragTyp;
+	}
+
+	void setVertragTyp(String vertragTyp) {
+		this.vertragTyp = vertragTyp;
+	}
+	
+}
